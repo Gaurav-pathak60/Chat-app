06e1fa967c69b03e694d7c8463a99ed1f4b430f4
Gaurav Pathak <gauravpathak9058@gmail.com>
2024-03-10 22:49:17 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory
+    emptyOutDir: true // Clear the output directory before building
+  },
+  server: {
+    // Serve files from the public directory
+    fs: {
+      allow: ['..'] // Allow access to the public directory
+    }
+  }
+});
